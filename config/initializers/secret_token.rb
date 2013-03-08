99d03e71813d5ebd1c2487e754211c39df008243
Mihir <mihir@5ve.in>
2013-03-08 15:11:41 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codetada::Application.config.secret_token = '84ae7cbc6723b1aca7e9c094c600a5ba46cb8766eb03c4eb0b2116189fce802472322eafcd9674ee36e53144201f3c455bb0b8ac85e97611d4f86f6bea0d093b'
